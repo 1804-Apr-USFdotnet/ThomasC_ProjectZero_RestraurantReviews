11a2cc473550a6bbde3609b6945107556ea05bd2
Chris Thomas <webmaster102@gmail.com>
2018-04-22 14:52:36 -0400

    readded Test project

---
+++ RestaurantRatings/RestaurantTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestaurantTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
