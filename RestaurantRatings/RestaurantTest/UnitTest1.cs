ec56ae4218fcb4447c1be4ca48872168143c5075
Chris Thomas <webmaster102@gmail.com>
2018-04-22 00:21:27 -0400

    added test project

---
+++ RestaurantRatings/RestaurantTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestaurantTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
