38971991d975f280064949aca4bbc8c0ba02c105
Chris Thomas <webmaster102@gmail.com>
2018-04-23 11:27:10 -0400

    challenge added

---
+++ CodeChallenge/PalindromeLibrary/Palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PalindromeLibrary
+{
+    public class Palindrome
+    {
+        public Palindrome()
+        {
+        }
+
+        public static bool IsPalindrome(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+    }
+}
---
+++ CodeChallenge/PalindromeTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+using System;
+using Palindrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PalindromeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            [TestMethod]
+            public void TestMethod1()
+            {
+                bool output = Palindrome.Palindrome.IsPalindrome("racecar");
+                Assert.IsTrue(output);
+            }
+
+            [TestMethod]
+            public void TestMethod2()
+            {
+                bool output = Palindrome.Palindrome.IsPalindrome("Racecar");
+                Assert.IsTrue(output);
+            }
+
+            [TestMethod]
+            public void TestMethod3()
+            {
+                bool output = Palindrome.Palindrome.IsPalindrome("1221");
+                Assert.IsTrue(output);
+            }
+
+            [TestMethod]
+            public void TestMethod4()
+            {
+                bool output = Palindrome.Palindrome.IsPalindrome("never Odd, or Even");
+                Assert.IsTrue(output);
+            }
+        }
+    }
+}
