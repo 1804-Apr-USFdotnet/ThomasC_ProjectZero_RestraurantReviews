da28c3a179867840a873dfc84c5e70894730f49e
Chris Thomas <webmaster102@gmail.com>
2018-04-30 00:09:48 -0400

    added test files

---
+++ RestaurantReviews/RestaurantTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantBL;
+using RestaurantDL;
+
+namespace RestaurantTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Restaurant restaurant = new Restaurant();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string expected = "Tampa, FL";
+
+            //act
+            var actual = restaurant.address;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
